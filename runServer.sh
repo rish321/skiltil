fe3efe091c489c7ba2e0be9695bbb1565001ad26
rishabh <ris.90s@gmail.com>
2016-12-01 13:27:52 +0000

    merged changes

--- runServer.sh
+++ runServer.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-python manage.py runserver 0.0.0.0:80 --insecure
-=======
-python manage.py runserver 80
->>>>>>> 129bfef3c2ab5c59bfd2756e37d33c16224ee6f4
+python manage.py runserver 0.0.0.0:80 --insecure >> runserver &

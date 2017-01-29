afbcb760f51f61a0b23022120d6872fb0e8bd850
rish321 <ris.90s@gmail.com>
2017-01-30 01:16:08 +0530

    increased popup width added code in search and list

--- proj/admin.py
+++ proj/admin.py
@@ -6,16 +6,16 @@ from .models import SkillTopic, Skill, CustomerRequest
 
 @admin.register(SkillTopic)
 class SkillTopicAdmin(admin.ModelAdmin):
-	list_display = ['topic_name', 'clicks', 'classes_given']
-	search_fields = ['topic_name']
+	list_display = ['topic_code', 'topic_name', 'clicks', 'classes_given']
+	search_fields = ['topic_code', 'topic_name']
 	readonly_fields = ['topic_code', 'clicks', 'classes_given']
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
 	raw_id_fields = ["topic"]
 	list_filter = ["no_teachers", "topic"]
-	list_display = ["skill_name", "topic", "clicks", "classes_given", "no_teachers"]
-	search_fields = ["skill_name", "topic__topic_name"]
+	list_display = ['skill_code', "skill_name", "topic", "clicks", "classes_given", "no_teachers"]
+	search_fields = ['skill_code', "skill_name", "topic__topic_name"]
 	readonly_fields = ['skill_code', 'clicks', 'classes_given', 'no_teachers']
 
 #admin.site.register(SkillTopic)
